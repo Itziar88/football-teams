50fad27ecb39290f0075e35f77f1fd7bad3a431e
Itziar Puebla <itziarpuebla@gmail.com>
2020-05-01 18:22:34 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Teams</router-link> |
+      <router-link to="/favouriteTeams">Favourite Teams</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Teams.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="Teams">
+    <div v-if="teams.length !== 0">
+      <div
+        v-for="team in teams"
+        :key="team.id"
+      >
+        <b-card
+          :title="team.school"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </b-card-text>
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+        {{ teams }}
+      </div>
+    </div>
+    <div v-else>
+      Loading...
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Teams',
+  data () {
+    return {
+      teams: null
+    }
+  },
+  mounted () {
+    this.getTeams()
+  },
+  methods: {
+    async getTeams () {
+      await axios.get('https://api.collegefootballdata.com/teams').then(response => { this.teams = response.data })
+    }
+  }
+}
+</script>
